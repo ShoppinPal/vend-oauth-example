f6d2e8cb53bf6e7da81159f79fc763b7835cf627
Megha Desai <desaimegha14@gmail.com>
2015-06-17 21:25:06 -0400

    Try and error with grunt install

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,5 @@
 !/bin/bash
 ./node_modules/bower/bin/bower install
 ./node_modules/grunt-cli/bin/grunt server
+./node_modules/grunt-cli/bin/grunt jshint
+./node_modules/grunt-cli/bin/grunt loadConfig:development
